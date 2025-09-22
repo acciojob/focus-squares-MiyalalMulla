2d1f39a9afe45d8c534617b59538729eb7c8c341
MiyalalMulla <mullamiyalal@gmail.com>
2025-09-22 11:40:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const myDivs = document.querySelectorAll(".square");
+myDivs.forEach((myDiv) => {
+    myDiv.addEventListener("mouseover", () => {
+        myDivs.forEach((e) => {
+            if(e==myDiv){
+				  e.style.backgroundColor = "#E6E6FA";
+			}
+			else{
+				e.style.backgroundColor = "#6F4E37"
+			}
+        });
+		
+    });
+	myDiv.addEventListener("mouseout", () => {
+        myDivs.forEach((e) => {
+            e.style.backgroundColor = "#E6E6FA";
+        });
+    });
+});
